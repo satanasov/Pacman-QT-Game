e5059c25776e6b312e518e2be9bd48311d80e94a
Stanislav Atanasov <lucifer@anavaro.com>
2020-06-03 11:14:13 +0300

    Some code visualization

--- textdrawing.cpp
+++ textdrawing.cpp
@@ -33,50 +33,50 @@ void TextDrawing::paint(QPainter *painter, const QStyleOptionGraphicsItem *optio
 
     if(!playing){
         font.setPointSize ( 18 );
-         painter->setFont(font);
+        painter->setFont(font);
 
 
         if(over){
             font.setPointSize ( 32 );
             QPen pen(Qt::red);
             painter->setPen(pen);
-             painter->setFont(font);
-             painter->drawText( x+80,y-180, "GAME OVER" );
-             font.setPointSize ( 18 );
-             QPen highPen(Qt::yellow);
-             painter->setPen(highPen);
-              painter->setFont(font);
-             painter->drawText( x+120,y-110, "HIGHSCORE" );
-             painter->drawText( x+0,y-70, "Tom" );
-             painter->drawText( x+80,y-70, QString::number(score+1000));
-             painter->drawText( x+140,y-70, "Ultra Hard" );
-              painter->drawText( x+250,y-70, "1000 Seconds" );
-             painter->drawText( x+0,y-50, name );
-             painter->drawText( x+80,y-50, QString::number(score));
-             painter->drawText( x+140,y-50, difficulty );
-              painter->drawText( x+250,y-50, QString::number(timeElapsed)+" Seconds" );
-             painter->drawText( x+0,y-30, "Jeff" );
-             painter->drawText( x+80,y-30, QString::number(score-10));
-             painter->drawText( x+140,y-30, difficulty );
-             painter->drawText( x+250,y-30, "53 Seconds" );
-             painter->drawText( x+0,y-10, "AAA" );
-             painter->drawText( x+80,y-10, QString::number(score-100));
-             painter->drawText( x+140,y-10, "medium" );
-             painter->drawText( x+250,y-10, "89 Seconds" );
-             painter->drawText( x+0,y+10, "Doug" );
-             painter->drawText( x+80,y+10, QString::number(score-200));
-             painter->drawText( x+140,y+10, "easy" );
-             painter->drawText( x+250,y+10, "100 Seconds" );
-//              painter->drawText( x+80,y-300, "Press Space to play again" );
+            painter->setFont(font);
+            painter->drawText( x+80,y-180, "GAME OVER" );
+            font.setPointSize ( 18 );
+            QPen highPen(Qt::yellow);
+            painter->setPen(highPen);
+            painter->setFont(font);
+            painter->drawText( x+120,y-110, "HIGHSCORE" );
+            painter->drawText( x+0,y-70, "Tom" );
+            painter->drawText( x+80,y-70, QString::number(score+1000));
+            painter->drawText( x+140,y-70, "Ultra Hard" );
+            painter->drawText( x+250,y-70, "1000 Seconds" );
+            painter->drawText( x+0,y-50, name );
+            painter->drawText( x+80,y-50, QString::number(score));
+            painter->drawText( x+140,y-50, difficulty );
+            painter->drawText( x+250,y-50, QString::number(timeElapsed)+" Seconds" );
+            painter->drawText( x+0,y-30, "Jeff" );
+            painter->drawText( x+80,y-30, QString::number(score-10));
+            painter->drawText( x+140,y-30, difficulty );
+            painter->drawText( x+250,y-30, "53 Seconds" );
+            painter->drawText( x+0,y-10, "AAA" );
+            painter->drawText( x+80,y-10, QString::number(score-100));
+            painter->drawText( x+140,y-10, "medium" );
+            painter->drawText( x+250,y-10, "89 Seconds" );
+            painter->drawText( x+0,y+10, "Doug" );
+            painter->drawText( x+80,y+10, QString::number(score-200));
+            painter->drawText( x+140,y+10, "easy" );
+            painter->drawText( x+250,y+10, "100 Seconds" );
+            //painter->drawText( x+80,y-300, "Press Space to play again" );
 
         }else{
 
-              painter->drawText( boundingRect(),Qt::AlignCenter, "PRESS SPACE TO START" );
+            painter->drawText( boundingRect(),Qt::AlignCenter, "PRESS SPACE TO START" );
             font.setPointSize ( 32 );
             QPen pen(Qt::yellow);
             painter->setPen(pen);
-             painter->setFont(font);
-             painter->drawText( x+100,y-180, "Pac Man" );
+            painter->setFont(font);
+            painter->drawText( x+100,y-180, "Pac Man" );
 
 
 
